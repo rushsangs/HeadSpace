932223a49f700045634bf3a8123dbf0e01c1c848
Rush Sangs <rush.sang@gmail.com>
2018-06-01 10:22:09 -0600

    added representation for desire and belief

---
+++ NarrativePlanning/NarrativePlanning/Desire.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NarrativePlanning
+{
+    class Desire
+    {
+        /// <summary>
+        /// The Character asically stores tyhe name and ONE literal 
+        /// in either the B+, B- or Unsure.
+        /// </summary>
+        Character character
+        {
+            get;
+            set;
+        }
+        List<String> motivations
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ NarrativePlanning/NarrativePlanning/Intention.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NarrativePlanning
+{
+    class Intention
+    {
+
+        /// <summary>
+        /// Must store the character name and the single literal
+        /// which can be either B plus, B minues, or Unsure
+        /// </summary>
+        Character character
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// WorldState where this intention was adopted
+        /// </summary>
+        WorldState state
+        {
+            get;
+            set;
+        }
+
+        List<String> motivations
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The plan that the character can come up with 
+        /// to achieve the goal.
+        /// </summary>
+        Plan plan
+        {
+            get;
+            set;
+        }
+
+    }
+}
